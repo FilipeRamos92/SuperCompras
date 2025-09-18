1348f2454057018a9b9498520dd2863caaf01091
Filipe <filipe.ramos92@hotmail.com>
2025-09-17 21:59:03 -0300

    Alinhando e separando componentes em linhas e colunas

--- app/src/main/java/com/example/supercompras/MainActivity.kt
+++ app/src/main/java/com/example/supercompras/MainActivity.kt
@@ -5,10 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
@@ -18,9 +20,11 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.supercompras.ui.theme.Marinho
@@ -34,7 +38,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             SuperComprasTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column {
+                    Column(verticalArrangement = Arrangement.Top) {
                         ImagemTopo(modifier = Modifier.padding(innerPadding))
                         Titulo(
                             texto = "Lista de Compras", modifier = Modifier.padding(innerPadding)
@@ -56,21 +60,40 @@ fun Titulo(texto: String, modifier: Modifier = Modifier) {
 
 @Composable
 fun ItemDaLista(modifier: Modifier = Modifier) {
-    Row {
-        Checkbox(
-            checked = false,
-            onCheckedChange = null,
-            modifier = modifier
-                .padding(end = 16.dp)
-        )
-        Text(
-            text = "Suco",
-            style = Typography.bodyMedium,
-            modifier = modifier
-        )
-        Icone(icone = Icons.Default.Delete, modifier = modifier)
-        Icone(icone = Icons.Default.Edit, modifier = modifier)
+    Column(verticalArrangement = Arrangement.Top, modifier = modifier) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start,
+            modifier = Modifier
+        ) {
+            Checkbox(
+                checked = false,
+                onCheckedChange = { },
+                modifier = Modifier
+                    .padding(end = 8.dp)
+                    .requiredSize(24.dp)
+            )
+            Text(
+                text = "Suco",
+                style = Typography.bodyMedium,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+            )
+            Icone(
+                icone = Icons.Default.Delete,
+                modifier = Modifier
+                    .padding(end = 8.dp)
+                    .size(16.dp)
+            )
+            Icone(
+                icone = Icons.Default.Edit,
+                modifier = Modifier
+                    .size(16.dp)
+            )
+        }
+        Text(text = "Segunda-feira (31/10/2022) às 08:30")
     }
+
 }
 
 @Composable
