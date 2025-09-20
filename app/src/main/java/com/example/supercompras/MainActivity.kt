1578eb0d860ea69be81a1b1d34723591a6a8ca1e
Filipe <filipe.ramos92@hotmail.com>
2025-09-20 16:45:02 -0300

    Adicionando botão e reposicionando elementos na tela

--- app/src/main/java/com/example/supercompras/MainActivity.kt
+++ app/src/main/java/com/example/supercompras/MainActivity.kt
@@ -9,27 +9,34 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.supercompras.ui.theme.Coral
 import com.example.supercompras.ui.theme.Marinho
 import com.example.supercompras.ui.theme.SuperComprasTheme
 import com.example.supercompras.ui.theme.Typography
@@ -41,8 +48,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             SuperComprasTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(verticalArrangement = Arrangement.Top, modifier = Modifier.padding(innerPadding)) {
+                    Column(
+                        verticalArrangement = Arrangement.Top,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
                         ImagemTopo()
+                        AdicionarItem()
+                        BotaoAdicionarItem(onClick = {})
                         Titulo(texto = "Lista de Compras")
                         ItemDaLista()
                         Titulo(texto = "Comprado")
@@ -56,13 +69,41 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun AdicionarItem(modifier: Modifier = Modifier) {
-    var texto = remember { mutableStateOf("") }
+    var texto = rememberSaveable { mutableStateOf("") }
 
-    TextField(
-        value = "",
+    OutlinedTextField(
+        value = texto.value,
         onValueChange = { texto.value = it },
+        placeholder = {
+            Text(
+                text = "Digite o item que deseja adicionar",
+                style = Typography.bodyMedium,
+                color = Color.Gray
+            )
+        },
+        shape = RoundedCornerShape(24.dp),
+        singleLine = true,
         modifier = modifier
+            .padding(8.dp)
+            .fillMaxWidth(),
     )
+}
+
+@Composable
+fun BotaoAdicionarItem(onClick: () -> Unit, modifier: Modifier = Modifier) {
+    Button(
+        onClick = onClick,
+        shape = RoundedCornerShape(24.dp),
+        colors = ButtonDefaults.buttonColors(containerColor = Coral),
+        modifier = modifier
+    ) {
+        Text(
+            text = "Salvar item",
+            color = Color.White,
+            modifier = Modifier
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+        )
+    }
 }
 
 @Composable
@@ -89,19 +130,15 @@ fun ItemDaLista(modifier: Modifier = Modifier) {
                 text = "Suco",
                 style = Typography.bodyMedium,
                 textAlign = TextAlign.Start,
-                modifier = Modifier
-                    .weight(1f)
+                modifier = Modifier.weight(1f)
             )
             Icone(
-                icone = Icons.Default.Delete,
-                modifier = Modifier
+                icone = Icons.Default.Delete, modifier = Modifier
                     .padding(end = 8.dp)
                     .size(16.dp)
             )
             Icone(
-                icone = Icons.Default.Edit,
-                modifier = Modifier
-                    .size(16.dp)
+                icone = Icons.Default.Edit, modifier = Modifier.size(16.dp)
             )
         }
         Text(
@@ -156,6 +193,14 @@ private fun ImagemTopoPreview() {
 private fun ItemDaListaPreview() {
     SuperComprasTheme {
         ItemDaLista()
+    }
+}
+
+@Preview
+@Composable
+private fun BotaoAdicionarItemPreview() {
+    SuperComprasTheme {
+        BotaoAdicionarItem(onClick = {})
     }
 }
 
